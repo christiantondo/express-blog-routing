ae51dd522d8f66fb599bdcb37d7083c06e387031
christiantondo <christiantondo1@gmail.com>
2026-02-25 20:04:41 +0100

    routers

--- app.js
+++ app.js
@@ -1,44 +1,17 @@
 
 const express = require('express')
+const postsRouter = require('./routers/posts')
+
 const app = express()
 const port = 3000
 
-const posts = require('./data/posts')
 
 // Main page
 app.get('/', (req, res) => {
     res.send('Main page')
 });
 
-// Index (cRud)
-app.get('/posts/', (req, res) => {
-    res.json(posts)
-});
-
-// Show (cRud)
-app.get('/posts/:id', (req, res) => {
-    res.send(`You requested to SHOW the post with id: ${req.params.id}`)
-});
-
-// Store (Crud)
-app.post('/posts/', (req, res) => {
-    res.send(`You requested to CREATE a new post`)
-});
-
-// Update (crUd)
-app.put('/posts/:id', (req, res) => {
-    res.send(`You requested to UPDATE (complete) the post with id: ${req.params.id}`)
-});
-
-// Modify (crUd)
-app.patch('/posts/:id', (req, res) => {
-    res.send(`You requested to MODIFY (partial) the post with id: ${req.params.id}`)
-});
-
-// Destroy (cruD)
-app.delete('/posts/:id', (req, res) => {
-    res.send(`You requested to DELETE the post with id: ${req.params.id}`)
-});
+app.use('/posts', postsRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
---
+++ routers/posts.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const posts = require('../data/posts')
+
+// Index (cRud)
+router.get('/', (req, res) => {
+    res.json(posts)
+});
+
+// Show (cRud)
+router.get('/:id', (req, res) => {
+    res.send(`You requested to SHOW the post with id: ${req.params.id}`)
+});
+
+// Store (Crud)
+router.post('/', (req, res) => {
+    res.send(`You requested to CREATE a new post`)
+});
+
+// Update (crUd)
+router.put('/:id', (req, res) => {
+    res.send(`You requested to UPDATE (complete) the post with id: ${req.params.id}`)
+});
+
+// Modify (crUd)
+router.patch('/:id', (req, res) => {
+    res.send(`You requested to MODIFY (partial) the post with id: ${req.params.id}`)
+});
+
+// Destroy (cruD)
+router.delete('/:id', (req, res) => {
+    res.send(`You requested to DELETE the post with id: ${req.params.id}`)
+});
+
+module.exports = router;
